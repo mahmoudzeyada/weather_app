28dbf3c9053eac5bb18870ae831d6371ac5b6f23
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-09-02 16:54:45 +0200

    adding the logic for requesting weather information

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
 console.log('loading client js code');
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    messageOne.textContent = '...Loading';
+    messageTwo.textContent = '';
+
+    fetch('http://127.0.0.1:3000/weather?address=' + search.value).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                console.log(data);
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        });
+    });
+});
+
